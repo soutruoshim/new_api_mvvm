97bec5670f0d18254e2534834caaaa20871c1d87
soutruoshim <sout.rahim@gmail.com>
2023-04-02 21:58:54 +0700

    nav host fragement

--- app/src/main/java/com/srhdp/newapimvvm/MainActivity.kt
+++ app/src/main/java/com/srhdp/newapimvvm/MainActivity.kt
@@ -2,10 +2,19 @@ package com.srhdp.newapimvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.srhdp.newapimvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvNews.setupWithNavController(navController)
     }
 }
